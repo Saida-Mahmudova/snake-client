37ba51b3e2995f17faeb5426fa724c0424c6651c
Saida-Mahmudova <mahmudovas.au@gmail.com>
2022-03-17 01:43:58 +0300

    set keys for movement

--- input.js
+++ input.js
@@ -1,4 +1,6 @@
-const setupInput = function () {
+let connection;
+const setupInput = (conn) => {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -10,7 +12,19 @@ const setupInput = function () {
 const handleUserInput = function (data) {
   if (data === '\u0003') {
     process.exit();
+  } else if (data === "w") {
+    console.log("Move:up")
+    connection.write('Move: up');
+  } else if (data === "a") {
+    console.log("Move: left")
+    connection.write('Move: left');
+  } else if (data === "s") {
+    console.log("Move: down")
+    connection.write("Move: down");
+  } else if (data === "d") {
+    console.log("Move: right")
+    connection.write("Move: right");
   }
 };
 
-module.exports = { setupInput }
+module.exports = setupInput;
--- play.js
+++ play.js
@@ -1,8 +1,9 @@
 const connect = require('./client');
-const { setupInput } = require('./input')
+const setupInput = require('./input')
 // establishes a connection with the game server
 
 
 console.log("Connecting ...");
-connect();
-setupInput();
+let connection = connect();
+
+setupInput(connection);
